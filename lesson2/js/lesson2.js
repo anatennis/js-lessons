5b721cf5bebd5d899322d09120b8ea19bc874eab
anaroman <romacia2007@mail.ru>
2018-10-13 17:21:32 +0300

    2d

---
+++ lesson2/js/lesson2.js
@@ -0,0 +1,85 @@
+/*console.log("Констуркции ветвлени")
+
+let isActive =true;
+
+if (isActive) {
+    isActive =false;
+}
+
+console.log("elem isActive", isActive);
+
+let season = 'summer';
+if (season === 'summer') {
+    document.body.style.background = 'gray';
+}
+else if (season === 'winter') {
+    document.body.style.background = 'blue';
+}
+
+
+switch ( ) {
+    case
+
+    [break];
+
+    case
+
+    [break];
+
+
+    default:
+
+}
+
+console.log("Cycles");
+
+let pwd = prompt("Password");
+
+while  (pwd !== 'admin') {
+    pwd = prompt("Input right password");
+}
+
+console.log("Welcome, admin");
+
+let a =2;
+
+do {
+    console.log("a =", a);
+    a--;
+} while (a);
+
+let newsCount =6;
+
+for (let i = 1; i<=newsCount; i++) {
+    if (i%2) continue;
+    console.log("News #", i);
+}
+
+a = null;
+let b = 0;
+let c = "";
+let d = "Smth";
+let f = "Smth2";
+let res = null || 0 || "" || "Smth" || "Smth2";
+
+console.log(res);*/
+
+console.log("Arrays");
+let emptyArr = [];
+let countries = ['sum', 'wint', 'aut', 'spr'];
+
+console.log(countries);
+
+for (i=0; i<countries.length; i++) {
+    console.log(countries[i]);
+}
+
+countries.pop();
+/*
+let pics = ['pic1.jpg', 'pic2.jpg','pic3.jpg']
+
+for (i=0; i<=pics.length; i++) {
+let img = document.createElement('img');
+img.setAttribute('src', 'img/' + pics[i]);
+documeny.getElementById('pics').appendChild(img);
+}*/
